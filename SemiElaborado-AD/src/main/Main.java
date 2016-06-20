6ba06547bc751c28a86296a6341592718b6b8c08
martinh <fmartinhuber@gmail.com>
2016-06-19 22:44:04 -0300

    a

--- SemiElaborado-AD/src/main/Main.java
+++ SemiElaborado-AD/src/main/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		Controlador controlado = new Controlador ();
 		
+		
 		for (SemiElaboradoBean p: controlado.semiElaboradosProductos()) {
 			System.out.println(p.toString());
 		}
